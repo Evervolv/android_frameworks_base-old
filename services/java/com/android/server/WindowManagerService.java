8003d723cc8ccb91961e8524498ab823a6eefd30
andrew.boren <andrew.boren@gmail.com>
2011-04-02 22:48:39 -0700

    Update from CM.
    Change-Id: I453b23bb7884b47bd3923410caaccefec74160e3

--- services/java/com/android/server/WindowManagerService.java
+++ services/java/com/android/server/WindowManagerService.java
@@ -5421,7 +5421,6 @@ public class WindowManagerService extends IWindowManager.Stub
         int deviceId = ev.getDeviceId();
         int scancode = ev.getScanCode();
         int source = ev.getSource();
-        int flags = ev.getFlags();
         
         if (source == InputDevice.SOURCE_UNKNOWN) {
             source = InputDevice.SOURCE_KEYBOARD;
@@ -5431,7 +5430,7 @@ public class WindowManagerService extends IWindowManager.Stub
         if (downTime == 0) downTime = eventTime;
 
         KeyEvent newEvent = new KeyEvent(downTime, eventTime, action, code, repeatCount, metaState,
-                deviceId, scancode, flags | KeyEvent.FLAG_FROM_SYSTEM, source);
+                deviceId, scancode, KeyEvent.FLAG_FROM_SYSTEM, source);
 
         final int pid = Binder.getCallingPid();
         final int uid = Binder.getCallingUid();
