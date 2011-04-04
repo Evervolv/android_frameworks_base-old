2e2d8757b54d617cf7dd63f51e829942525daadf
andrew.boren <andrew.boren@gmail.com>
2011-04-03 17:17:59 -0700

    Update from CM.
    Change-Id: I900c2a74d37cf9952c8dfe6861cc7c3c7da89464

--- core/java/com/android/internal/os/ZygoteInit.java
+++ core/java/com/android/internal/os/ZygoteInit.java
@@ -95,7 +95,7 @@ public class ZygoteInit {
     private static final String PRELOADED_CLASSES = "preloaded-classes";
 
     /** Controls whether we should preload resources during zygote init. */
-    private static final boolean PRELOAD_RESOURCES = true;
+    private static final boolean PRELOAD_RESOURCES = false;
 
     /**
      * List of methods we "warm up" in the register map cache.  These were
@@ -407,6 +407,8 @@ public class ZygoteInit {
                 N = preloadColorStateLists(runtime, ar);
                 Log.i(TAG, "...preloaded " + N + " resources in "
                         + (SystemClock.uptimeMillis()-startTime) + "ms.");
+            } else {
+                Log.i(TAG, "Preload resources disabled, skipped.");
             }
             mResources.finishPreloading();
         } catch (RuntimeException e) {
@@ -511,7 +513,7 @@ public class ZygoteInit {
         String args[] = {
             "--setuid=1000",
             "--setgid=1000",
-            "--setgroups=1001,1002,1003,1004,1005,1006,1007,1008,1009,1010,3001,3002,3003",
+            "--setgroups=1001,1002,1003,1004,1005,1006,1007,1008,1009,1010,1300,3001,3002,3003",
             "--capabilities=130104352,130104352",
             "--runtime-init",
             "--nice-name=system_server",
