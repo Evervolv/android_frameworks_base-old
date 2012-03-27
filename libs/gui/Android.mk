04ec72419384cdb6608078030b54bffe9ff5263d
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-03-27 00:06:47 -0500

    qui: qsd8k: skip S3DFormat update, not supported anyway
    Change-Id: I15a053dce2c2ae4f43f1e3cbd171475e8f13da0b

--- libs/gui/Android.mk
+++ libs/gui/Android.mk
@@ -30,16 +30,24 @@ LOCAL_SHARED_LIBRARIES := \
 	libGLESv2
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+
 ifneq ($(BOARD_USES_LEGACY_QCOM),true)
 LOCAL_SHARED_LIBRARIES += \
         libQcomUI
 LOCAL_C_INCLUDES := hardware/qcom/display/libqcomui
 LOCAL_CFLAGS += -DQCOM_HARDWARE
-endif
+
+endif # !LEGACY_QCOM
+
 ifeq ($(TARGET_QCOM_HDMI_OUT),true)
 LOCAL_CFLAGS += -DQCOM_HDMI_OUT
 endif
+
+ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+LOCAL_CFLAGS += -DTARGET8x50
 endif
+
+endif # QCOM_HARDWARE
 
 LOCAL_MODULE:= libgui
 
