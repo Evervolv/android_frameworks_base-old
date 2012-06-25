9a531418fd40467af28dbfc1c72ec18203c3774a
Brice Jaglin <brice.jaglin@sonyericsson.com>
2012-06-25 21:51:39 +0900

    Fixed upgrading from forward-lock application to system application
    Fixes the case when the app on system is newer than the
    currently installed. Something that can happen e.g. after
    a FOTA update.
    
    Change-Id: I102e9cdd5693d5e66667c0c8989dc2643c72dd16

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -3677,6 +3677,7 @@ public class PackageManagerService extends IPackageManager.Stub {
             updatedPkg = mSettings.getDisabledSystemPkgLPr(ps != null ? ps.name : pkg.packageName);
             if (DEBUG_INSTALL && updatedPkg != null) Slog.d(TAG, "updatedPkg = " + updatedPkg);
         }
+        boolean updatedPkgBetter = false;
         // First check if this is a system package that may involve an update
         if (updatedPkg != null && (parseFlags&PackageParser.PARSE_IS_SYSTEM) != 0) {
             if (ps != null && !ps.codePath.equals(scanFile)) {
@@ -3731,6 +3732,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                     synchronized (mPackages) {
                         mSettings.enableSystemPackageLPw(ps.name);
                     }
+                    updatedPkgBetter = true;
                 }
             }
         }
@@ -3807,7 +3809,7 @@ public class PackageManagerService extends IPackageManager.Stub {
 
         String codePath = null;
         String resPath = null;
-        if ((parseFlags & PackageParser.PARSE_FORWARD_LOCK) != 0) {
+        if ((parseFlags & PackageParser.PARSE_FORWARD_LOCK) != 0 && !updatedPkgBetter) {
             if (ps != null && ps.resourcePathString != null) {
                 resPath = ps.resourcePathString;
             } else {
