2f4946fc42aad2de3a4a6e1f9d44ee46f476aa99
Arne Coucheron <arco68@gmail.com>
2012-07-14 02:48:48 +0000

    Process the BTN_MOUSE event
    This is required in order to make the trackball on devices that has it to
    work properly, like it does before ICS. In addition to this patch, you just
    need an .idc file that matches the device name for your the trackball. You can
    find it by either looking in a kernel log or a logcat.
    
    For example, a device with a Crucial optical navigation pad, will be called
    curcial-oj.idc
    
    The .idc file should contain the following:
    
    cursor.mode = navigation
    cursor.orientationAware = 1
    
    Place the file in /system/usr/idc folder.
    
    Enable in BoardConfig.mk with BOARD_USE_LEGACY_TRACKPAD := true
    
    Change-Id: I57e9912b9e4d6242c980c99f4fd28b0d50f19bb8
    
    Conflicts:
    
    	services/input/Android.mk

--- services/input/Android.mk
+++ services/input/Android.mk
@@ -41,6 +41,11 @@ LOCAL_SHARED_LIBRARIES := \
 LOCAL_C_INCLUDES := \
     external/skia/include/core
 
+
+ifeq ($(BOARD_USE_LEGACY_TRACKPAD),true)
+LOCAL_CFLAGS += -DLEGACY_TRACKPAD
+endif
+
 LOCAL_MODULE:= libinput
 
 LOCAL_MODULE_TAGS := optional
