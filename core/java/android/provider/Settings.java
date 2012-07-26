fcdc622760abceb65c31cc5d91a7f085ff481b9b
andrew.boren <andrew.boren@gmail.com>
2012-07-25 21:48:06 -0700

    frameworks: Add inital Evervolv Toolbox setting.
     * DISABLE_TOOLBOX is going to be used to bring the users experience to
       as close to stock experience as possible.
    
    Change-Id: Ie106f0e16bccab4aa81abc64e92bf9751c21de20

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2618,6 +2618,17 @@ public final class Settings {
          */
         public static final String POINTER_SPEED = "pointer_speed";
 
+        /* START Constants for Evervolv Toolbox */
+
+        /**
+        * Disable all toolbox options. Allows user to achieve a more "stock"
+        * look and feel of android.
+        * @hide
+        */
+        public static final String DISABLE_TOOLBOX = "disable_toolbox";
+
+        /* END Constants for Evervolv Toolbox */
+
         /**
          * Whether lock-to-app will be triggered by long-press on recents.
          * @hide
