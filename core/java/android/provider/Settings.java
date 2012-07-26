812a2c4b9f1f83a3b4eba776a17685067931b213
andrew.boren <andrew.boren@gmail.com>
2012-07-25 21:48:06 -0700

    frameworks: Add inital Evervolv Toolbox setting.
     * DISABLE_TOOLBOX is going to be used to bring the users experience to
       as close to stock experience as possible.
    
    Change-Id: Ie106f0e16bccab4aa81abc64e92bf9751c21de20

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3577,6 +3577,17 @@ public final class Settings {
          * the setting value. See an example above.
          */
 
+        /* START Constants for Evervolv Toolbox */
+
+        /**
+         * Disable all toolbox options. Allows user to achieve a more "stock"
+         * look and feel of android.
+         * @hide
+         */
+        public static final String DISABLE_TOOLBOX = "disable_toolbox";
+
+        /* END Constants for Evervolv Toolbox */
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
@@ -3631,7 +3642,8 @@ public final class Settings {
             RINGTONE,
             LOCK_TO_APP_ENABLED,
             NOTIFICATION_SOUND,
-            ACCELEROMETER_ROTATION
+            ACCELEROMETER_ROTATION,
+            DISABLE_TOOLBOX
         };
 
         /**
