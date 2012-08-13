ab2caedd0f83ea80cd362b66ec3eccab9a3da0ca
Paul Mongold <pmongold@gmail.com>
2012-08-13 19:56:48 -0400

    wifi: don't avoid poor connections if mobile isn't connected
    Change-Id: I98d755a8b398150830891b9d7e18e0a889d8c783

--- wifi/java/android/net/wifi/WifiWatchdogStateMachine.java
+++ wifi/java/android/net/wifi/WifiWatchdogStateMachine.java
@@ -262,6 +262,8 @@ public class WifiWatchdogStateMachine extends StateMachine {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(
                 Context.CONNECTIVITY_SERVICE);
         sWifiOnly = (cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE) == false);
+        boolean noMobileConnection = (cm.getNetworkInfo(
+                ConnectivityManager.TYPE_MOBILE).isConnected() == false);
 
         // Watchdog is always enabled. Poor network detection & walled garden detection
         // can individually be turned on/off
@@ -270,8 +272,8 @@ public class WifiWatchdogStateMachine extends StateMachine {
         putSettingsBoolean(contentResolver, Settings.Secure.WIFI_WATCHDOG_ON, true);
 
         // Disable poor network avoidance, but keep watchdog active for walled garden detection
-        if (sWifiOnly) {
-            log("Disabling poor network avoidance for wi-fi only device");
+        if (sWifiOnly || noMobileConnection) {
+            log("Disabling poor network avoidance");
             putSettingsBoolean(contentResolver,
                     Settings.Secure.WIFI_WATCHDOG_POOR_NETWORK_TEST_ENABLED, false);
         }
