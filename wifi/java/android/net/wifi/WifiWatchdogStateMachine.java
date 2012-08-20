833d7ea90b11f0e10a8e05383a26a401d5e8bbc0
Paul Mongold <pmongold@gmail.com>
2012-08-20 14:16:03 -0400

    wifi: fix NPE on wifi-only devices
    introduced in commit ab2caedd0f83ea80cd362b66ec3eccab9a3da0ca
    
    Change-Id: I1a3a4629ef5b113e537125522449b7f8e9f81ec3

--- wifi/java/android/net/wifi/WifiWatchdogStateMachine.java
+++ wifi/java/android/net/wifi/WifiWatchdogStateMachine.java
@@ -262,7 +262,7 @@ public class WifiWatchdogStateMachine extends StateMachine {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(
                 Context.CONNECTIVITY_SERVICE);
         sWifiOnly = (cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE) == false);
-        boolean noMobileConnection = (cm.getNetworkInfo(
+        boolean noMobileConnection = sWifiOnly ? true : (cm.getNetworkInfo(
                 ConnectivityManager.TYPE_MOBILE).isConnected() == false);
 
         // Watchdog is always enabled. Poor network detection & walled garden detection
@@ -272,7 +272,7 @@ public class WifiWatchdogStateMachine extends StateMachine {
         putSettingsBoolean(contentResolver, Settings.Secure.WIFI_WATCHDOG_ON, true);
 
         // Disable poor network avoidance, but keep watchdog active for walled garden detection
-        if (sWifiOnly || noMobileConnection) {
+        if (noMobileConnection) {
             log("Disabling poor network avoidance");
             putSettingsBoolean(contentResolver,
                     Settings.Secure.WIFI_WATCHDOG_POOR_NETWORK_TEST_ENABLED, false);
