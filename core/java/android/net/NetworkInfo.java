616f9ba9b0196b639fea4ca380d465143b0bbff1
andrew.boren <andrew.boren@gmail.com>
2012-12-20 20:58:37 -0700

    Wimax: Squashed commit of the following:
    Port WimaxHelper and (the toggle) WimaxButton from CM7.
    
    Both WimaxHelper and WimaxButton are updated to support Samsung's
    (crespo4g, epicmtd) Wimax4GManager proprietary class, whose API has been
    somewhat-significantly changed in ICS.  Support for HTC's (supersonic,
    speedy) WimaxController is retained in WimaxHelper, although untested.
    
    Note that WimaxManagerConstants was previously misdocumented, confabulating
    the lookup key EXTRA_4G_STATE ("4g_state") with EXTRA_WIMAX_STATUS
    ("wimax_status"), which no longer exists.  This is also fixed.
    
    SQN: WiMAX: add support for Sequans WiMAX devices
    
    issue: 4G icon does not display when connected to wimax
    
    Change-Id: If199c9251a7abd679b048b5aba3f601b4bf9047e

--- core/java/android/net/NetworkInfo.java
+++ core/java/android/net/NetworkInfo.java
@@ -123,6 +123,8 @@ public class NetworkInfo implements Parcelable {
      */
     private boolean mIsAvailable;
 
+    private String mInterfaceName;
+
     /**
      * @param type network type
      * @deprecated
@@ -391,6 +393,20 @@ public class NetworkInfo implements Parcelable {
         }
     }
 
+    /**
+     * @hide
+     */
+    public void setInterface(String paramString) {
+        this.mInterfaceName = paramString;
+    }
+
+    /**
+     * @hide
+     */
+    public String getInterface() {
+        return this.mInterfaceName;
+    }
+
     @Override
     public String toString() {
         synchronized (this) {
@@ -431,6 +447,7 @@ public class NetworkInfo implements Parcelable {
             dest.writeInt(mIsRoaming ? 1 : 0);
             dest.writeString(mReason);
             dest.writeString(mExtraInfo);
+            dest.writeString(mInterfaceName);
         }
     }
 
@@ -453,6 +470,7 @@ public class NetworkInfo implements Parcelable {
                 netInfo.mIsRoaming = in.readInt() != 0;
                 netInfo.mReason = in.readString();
                 netInfo.mExtraInfo = in.readString();
+                netInfo.mInterfaceName = in.readString();
                 return netInfo;
             }
 
