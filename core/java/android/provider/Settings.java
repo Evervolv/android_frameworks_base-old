007aa56c3fdc9e9ddfea786887142266153d4bb9
Roman Birg <romanbirg@gmail.com>
2013-01-15 23:48:45 +0100

    Notification sound-to-vibration conversion toggle
    Allows to change the new feature of AOSP 4.2, where all notification are
    changed to vibrations on vibrate mode. Instead, allow the old behaviour
    to silence sound notifications and to only play vibrations if desired.
    
    Alle credits go to romanbb from AOKP, I just ported it.
    Original AOKP-Commit:
    https://github.com/AOKP/frameworks_base/commit/c1b2bb9b5d0c33e790973841bb2acaa5ea5d51fe
    
    Adapted to evervolv code by Dorian Snyder
    
    Change-Id: Id27dd2975d3383f6d7395a0ee995d7832e87ca66
    Signed-off-by: Dorian Snyder <dastin1015@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2480,6 +2480,14 @@ public final class Settings {
         public static final String EGG_MODE = "egg_mode";
 
         /**
+         * toggle to "fix" the following: (found in NotificationManagerService)
+         *  new in 4.2: if there was supposed to be a sound and we're in vibrate mode,
+         *  we always vibrate, even if no vibration was specified
+         * @hide
+         */
+        public static final String NOTIFICATION_CONVERT_SOUND_TO_VIBRATION = "convert_sound_to_vibration";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
