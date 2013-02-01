58d736b4d099a501ae29c9cb88f8372b6acf266b
Danny Baumann <dannybaumann@web.de>
2013-02-01 10:39:36 +0100

    @hide new setting and move it to appropriate place.
    Change-Id: I02b50de9ba5b4aeab38acf6f9225908a4079b05a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2385,6 +2385,14 @@ public final class Settings {
 		/* END Constants for Theme compatibility */
 
         /**
+         * toggle to "fix" the following: (found in NotificationManagerService)
+         *  new in 4.2: if there was supposed to be a sound and we're in vibrate mode,
+         *  we always vibrate, even if no vibration was specified
+         * @hide
+         */
+        public static final String NOTIFICATION_CONVERT_SOUND_TO_VIBRATION = "convert_sound_to_vibration";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -2681,14 +2689,6 @@ public final class Settings {
         @Deprecated
         public static final String WIFI_WATCHDOG_PING_TIMEOUT_MS =
             Secure.WIFI_WATCHDOG_PING_TIMEOUT_MS;
-
-        /**
-         * toggle to "fix" the following: (found in NotificationManagerService)
-         *  new in 4.2: if there was supposed to be a sound and we're in vibrate mode,
-         *  we always vibrate, even if no vibration was specified
-         */
-        public static final String NOTIFICATION_CONVERT_SOUND_TO_VIBRATION = "convert_sound_to_vibration";
-
     }
 
     /**
