00dac48b39c451a470762097381586bc7f7231ae
Stephen Smalley <sds@tycho.nsa.gov>
2013-04-12 14:49:38 -0400

    Replace SEAndroid with SELinux.
    Change-Id: Ibbe544a9f025d71ad416bc01cee2145b62d7b2d4
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>

--- core/java/com/android/internal/os/ZygoteConnection.java
+++ core/java/com/android/internal/os/ZygoteConnection.java
@@ -831,7 +831,7 @@ class ZygoteConnection {
     }
 
     /**
-     * Applies zygote security policy for SEAndroid information.
+     * Applies zygote security policy for SELinux information.
      *
      * @param args non-null; zygote spawner arguments
      * @param peer non-null; peer credentials
@@ -850,7 +850,7 @@ class ZygoteConnection {
         if (!(peerUid == 0 || peerUid == Process.SYSTEM_UID)) {
             // All peers with UID other than root or SYSTEM_UID
             throw new ZygoteSecurityException(
-                    "This UID may not specify SEAndroid info.");
+                    "This UID may not specify SELinux info.");
         }
 
         boolean allowed = SELinux.checkSELinuxAccess(peerSecurityContext,
@@ -859,7 +859,7 @@ class ZygoteConnection {
                                                      "specifyseinfo");
         if (!allowed) {
             throw new ZygoteSecurityException(
-                    "Peer may not specify SEAndroid info");
+                    "Peer may not specify SELinux info");
         }
 
         return;
