d197e4d9fb7eb59cc28b0dc6bd96cadc45ebc5eb
Adnan Begovic <adnan@cyngn.com>
2013-11-04 00:58:57 +0000

    frameworks: Add unlinked ringtone and notification volumes
    Port over linked volumes from 12.1
    
    Implement linked volumes and add upgrade path.
    
    Change-Id: Ibbeff656fcaaa4a2ff79de530b48b216df271b21
    
    frameworks: read VOLUME_LINK_NOTIFICATION from proper table
    
    Change-Id: I15e9caa18f307a51a732c7a59013bbbb234a7477
    Signed-off-by: Roman Birg <roman@cyngn.com>
    (cherry picked from commit 1b7cfa1716017e0d03c9693428c21d8e11c5dc0a)
    
    fix Notification volume slider linking
    
    Change-Id: I0f74485f5c8c2aa7698cf9ba1c60c2c8c967798d
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Improve SeekBarVolumizer behavior with unlinked streams
    
    - Fix notification stream enabling at 0 volume
    - Fix the notification slider being set when moving ringer stream
    
    Change-Id: If3705cc833c07f726f8ad7b1ec79a42e86e8ff22
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    base: Fix notification sounds for wifi only devices
    
    The linking of voice & notification doesn't take into account the
    fact that a device may not actually have voice capabilities.  Whenever
    checking whether or not to link notification and ring volumes, also
    verify that the device really is voice capable.
    
    Change-Id: Iae49302d3b5934d36b182e289e4975f5dd85b834
    
    frameworks: fix setting up linked notification stream on boot
    
    updateStreamVolumeAlias() was previously responsible for checking the
    linked notification variable and mapping the notification stream, but
    the setting to read whether the notifications were linked was read in
    after that method had run which was responsible for linking them.
    Extract the logic and re-run it again after reading the persisted
    settings.
    
    Change-Id: Ia62bf3ca072def54ec6ebf4f9e865d9c913e3bc4
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    fixup volume stream linking
    
    - Make the SeekBarVolumizer move notification and ringer streams when
     linked and moving ringer.
    
    - Fix initial state causing volume stream seek bar to be set to 0
      initially
    
    - Clean up AudioService initialization code
    
    Change-Id: Ia11a7a392e5eb3ef862147843e365d5f20eaf90a
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: Fix volume panel inconsistencies
    
    * Use xml drawables everywhere
    * Add muted notification icon
    * Use AOSP naming scheme for drawables
    
    Change-Id: I14a3af9a7aecf4828f81a8e39855608e452848d6
    
    Misc fixes
    
    Change-Id: Ia94b0a6655e52f5949f031360f525c871cd8df14
    Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>
    
    Use STREAM_RING on non-voice capable devices
    
    * That's what Google does.
    
    Change-Id: Iac88d161eb1dccbbb5c538d5e04d8d1fc43edef2
    (cherry picked from commit 981e89eb6b99c058a9b3bf533e6494ab44a4b241)
    
    VolumeDialog: Fix mismerge
    
    Fixes not disappearing cast pickers
    
    * Commit 568585b introduced a new LinearLayout for the volume rows
    * Remove unused booleans
    
    Change-Id: I0660d10872cc1098fdd67b618a20ea0f8e926e68
    
    Fix VOLUME_LINK_NOTIFICATION javadoc
    
    * Fixes checkapi:
      frameworks/base/core/java/android/provider/Settings.java:8920: error:
      Added field android.provider.Settings.Secure.VOLUME_LINK_NOTIFICATION [AddedField]
    
    Change-Id: I4da34de53ce23e90d5f2eff72ef0a15034f08ec1
    
    Change-Id: I7695aa15bc85e8ff19747ba7cd6700e190c7d988
    
    VolumeDialog: display default row when active row is notification
    
    Commit "frameworks: Add unlinked ringtone and notification volumes"
    introduced STREAM_NOTIFICATION. However, this stream type, although
    marked as important, was not added to the list in shouldBeVisibleH.
    As a result, the volume panel behavior of STREAM_NOTIFICATION deviates
    from other user-facing streams like media, call and ring.
    
    This change adds STREAM_NOTIFICATION so the behavior becomes consistent.
    
    Change-Id: I092c5bf0ae8cbee85af6adfa0da308dfdb60e66a
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2583,6 +2583,8 @@ public final class Settings {
 
             // At one time in System, then Global, but now back in Secure
             MOVED_TO_SECURE.add(Secure.INSTALL_NON_MARKET_APPS);
+
+            MOVED_TO_SECURE.add(Secure.VOLUME_LINK_NOTIFICATION);
         }
 
         @UnsupportedAppUsage
@@ -9013,6 +9015,14 @@ public final class Settings {
         private static final Validator TAP_GESTURE_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        private static final Validator VOLUME_LINK_NOTIFICATION_VALIDATOR = BOOLEAN_VALIDATOR;
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -9148,7 +9158,8 @@ public final class Settings {
             GLOBAL_ACTIONS_PANEL_ENABLED,
             AWARE_LOCK_ENABLED,
             AWARE_TAP_PAUSE_GESTURE_COUNT,
-            AWARE_TAP_PAUSE_TOUCH_COUNT
+            AWARE_TAP_PAUSE_TOUCH_COUNT,
+            VOLUME_LINK_NOTIFICATION
         };
 
         /**
@@ -9346,6 +9357,7 @@ public final class Settings {
             VALIDATORS.put(AWARE_TAP_PAUSE_GESTURE_COUNT, NON_NEGATIVE_INTEGER_VALIDATOR);
             VALIDATORS.put(AWARE_TAP_PAUSE_TOUCH_COUNT, NON_NEGATIVE_INTEGER_VALIDATOR);
             VALIDATORS.put(TAP_GESTURE, TAP_GESTURE_VALIDATOR);
+            VALIDATORS.put(VOLUME_LINK_NOTIFICATION, VOLUME_LINK_NOTIFICATION_VALIDATOR);
         }
 
         /**
--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -23,6 +23,7 @@ import static android.media.AudioManager.RINGER_MODE_VIBRATE;
 import static android.media.AudioManager.STREAM_ACCESSIBILITY;
 import static android.media.AudioManager.STREAM_ALARM;
 import static android.media.AudioManager.STREAM_MUSIC;
+import static android.media.AudioManager.STREAM_NOTIFICATION;
 import static android.media.AudioManager.STREAM_RING;
 import static android.media.AudioManager.STREAM_VOICE_CALL;
 import static android.view.View.ACCESSIBILITY_LIVE_REGION_POLITE;
@@ -309,8 +310,13 @@ public class VolumeDialogImpl implements VolumeDialog,
             addRow(AudioManager.STREAM_MUSIC,
                     R.drawable.ic_volume_media, R.drawable.ic_volume_media_mute, true, true);
             if (!AudioSystem.isSingleVolume(mContext)) {
-                addRow(AudioManager.STREAM_RING,
-                        R.drawable.ic_volume_ringer, R.drawable.ic_volume_ringer_mute, true, false);
+                if (Util.isVoiceCapable(mContext)) {
+                    addRow(AudioManager.STREAM_RING, R.drawable.ic_volume_ringer,
+                            R.drawable.ic_volume_ringer_mute, true, false);
+                } else {
+                    addRow(AudioManager.STREAM_RING, R.drawable.ic_volume_notification,
+                            R.drawable.ic_volume_notification_mute, true, false);
+                }
                 addRow(STREAM_ALARM,
                         R.drawable.ic_alarm, R.drawable.ic_volume_alarm_mute, true, false);
                 addRow(AudioManager.STREAM_VOICE_CALL,
@@ -846,6 +852,7 @@ public class VolumeDialogImpl implements VolumeDialog,
 
             if (row.defaultStream) {
                 return activeRow.stream == STREAM_RING
+                        || activeRow.stream == STREAM_NOTIFICATION
                         || activeRow.stream == STREAM_ALARM
                         || activeRow.stream == STREAM_VOICE_CALL
                         || activeRow.stream == STREAM_ACCESSIBILITY
@@ -975,10 +982,14 @@ public class VolumeDialogImpl implements VolumeDialog,
             final VolumeRow row = mRows.get(i);
             if (row.ss == null || !row.ss.dynamic) continue;
             if (!mDynamic.get(row.stream)) {
-                mRows.remove(i);
-                mDialogRowsView.removeView(row.view);
+                removeRow(row);
             }
         }
+    }
+
+    private void removeRow(VolumeRow volumeRow) {
+        mRows.remove(volumeRow);
+        mDialogRowsView.removeView(volumeRow.view);
     }
 
     protected void onStateChangedH(State state) {
@@ -1003,6 +1014,10 @@ public class VolumeDialogImpl implements VolumeDialog,
             }
         }
 
+        if (Util.isVoiceCapable(mContext)) {
+            updateNotificationRowH();
+        }
+
         if (mActiveStream != state.activeStream) {
             mPrevActiveStream = mActiveStream;
             mActiveStream = state.activeStream;
@@ -1019,6 +1034,16 @@ public class VolumeDialogImpl implements VolumeDialog,
 
     CharSequence composeWindowTitle() {
         return mContext.getString(R.string.volume_dialog_title, getStreamLabelH(getActiveRow().ss));
+    }
+
+    private void updateNotificationRowH() {
+        VolumeRow notificationRow = findRow(AudioManager.STREAM_NOTIFICATION);
+        if (notificationRow != null && mState.linkedNotification) {
+            removeRow(notificationRow);
+        } else if (notificationRow == null && !mState.linkedNotification) {
+            addRow(AudioManager.STREAM_NOTIFICATION, R.drawable.ic_volume_notification,
+                    R.drawable.ic_volume_notification_mute, true, false);
+        }
     }
 
     private void updateVolumeRowH(VolumeRow row) {
@@ -1038,19 +1063,22 @@ public class VolumeDialogImpl implements VolumeDialog,
         final boolean isSystemStream = row.stream == AudioManager.STREAM_SYSTEM;
         final boolean isAlarmStream = row.stream == STREAM_ALARM;
         final boolean isMusicStream = row.stream == AudioManager.STREAM_MUSIC;
-        final boolean isRingVibrate = isRingStream
-                && mState.ringerModeInternal == AudioManager.RINGER_MODE_VIBRATE;
+        final boolean isNotificationStream = row.stream == AudioManager.STREAM_NOTIFICATION;
+        final boolean isVibrate = mState.ringerModeInternal == AudioManager.RINGER_MODE_VIBRATE;
+        final boolean isRingVibrate = isRingStream && isVibrate;
         final boolean isRingSilent = isRingStream
                 && mState.ringerModeInternal == AudioManager.RINGER_MODE_SILENT;
         final boolean isZenPriorityOnly = mState.zenMode == Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS;
         final boolean isZenAlarms = mState.zenMode == Global.ZEN_MODE_ALARMS;
         final boolean isZenNone = mState.zenMode == Global.ZEN_MODE_NO_INTERRUPTIONS;
-        final boolean zenMuted = isZenAlarms ? (isRingStream || isSystemStream)
-                : isZenNone ? (isRingStream || isSystemStream || isAlarmStream || isMusicStream)
+        final boolean zenMuted =
+                isZenAlarms ? (isRingStream || isSystemStream || isNotificationStream)
+                : isZenNone ? (isRingStream || isSystemStream || isAlarmStream || isMusicStream || isNotificationStream)
                 : isZenPriorityOnly ? ((isAlarmStream && mState.disallowAlarms) ||
                         (isMusicStream && mState.disallowMedia) ||
                         (isRingStream && mState.disallowRinger) ||
                         (isSystemStream && mState.disallowSystem))
+                : isVibrate ? isNotificationStream
                 : false;
 
         // update slider max
