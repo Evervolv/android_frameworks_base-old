rmcc/android_frameworks_base

Add framework support for keyboard LEDs (Caps/FN)
Change-Id: I7a6989b53f15e86ced59e5fd31464a2334bbfd68

services/java/com/android/server/PowerManagerService.java
@@ -188,6 +188,8 @@ class PowerManagerService extends IPowerManager.Stub
     private LightsService.Light mButtonLight;
     private LightsService.Light mKeyboardLight;
     private LightsService.Light mAttentionLight;
+    private LightsService.Light mCapsLight;
+    private LightsService.Light mFnLight;
     private UnsynchronizedWakeLock mBroadcastWakeLock;
     private UnsynchronizedWakeLock mStayOnWhilePluggedInScreenDimLock;
     private UnsynchronizedWakeLock mStayOnWhilePluggedInPartialLock;
@@ -462,6 +464,8 @@ class PowerManagerService extends IPowerManager.Stub
         mButtonLight = lights.getLight(LightsService.LIGHT_ID_BUTTONS);
         mKeyboardLight = lights.getLight(LightsService.LIGHT_ID_KEYBOARD);
         mAttentionLight = lights.getLight(LightsService.LIGHT_ID_ATTENTION);
+        mCapsLight = lights.getLight(LightsService.LIGHT_ID_CAPS);
+        mFnLight = lights.getLight(LightsService.LIGHT_ID_FN);
 
         mHandlerThread = new HandlerThread("PowerManagerService") {
             @Override
@@ -2369,6 +2373,11 @@ class PowerManagerService extends IPowerManager.Stub
                     }
                     userActivity(SystemClock.uptimeMillis(), false, BUTTON_EVENT, true);
                 }
+                // If hiding keyboard, turn off leds
+                if (!visible) {
+                    setKeyboardLight(false, 1);
+                    setKeyboardLight(false, 2);
+                }
             }
         }
     }
@@ -2730,6 +2739,21 @@ class PowerManagerService extends IPowerManager.Stub
             }
         }
     }
+
+    public void setKeyboardLight(boolean on, int key) {
+        if (key == 1) {
+            if (on) 
+                mCapsLight.setColor(0x00ffffff);
+            else
+                mCapsLight.turnOff();
+        } else if (key == 2) {
+            if (on) 
+                mFnLight.setColor(0x00ffffff);
+            else
+                mFnLight.turnOff();
+        }
+    }
+
 
     SensorEventListener mProximityListener = new SensorEventListener() {
         public void onSensorChanged(SensorEvent event) {

Evervolv/android_frameworks_base-old

Change-Id: I0045745b8573410d3017c08e29a0122e7189c40a

services/java/com/android/server/power/PowerManagerService.java
@@ -188,6 +188,8 @@ public final class PowerManagerService extends IPowerManager.Stub
     private LightsService.Light mAttentionLight;
     private LightsService.Light mButtonsLight;
     private LightsService.Light mKeyboardLight;
+    private LightsService.Light mCapsLight;
+    private LightsService.Light mFnLight;
 
     private final Object mLock = new Object();
 
@@ -468,6 +470,8 @@ public final class PowerManagerService extends IPowerManager.Stub
             mAttentionLight = mLightsService.getLight(LightsService.LIGHT_ID_ATTENTION);
             mButtonsLight = mLightsService.getLight(LightsService.LIGHT_ID_BUTTONS);
             mKeyboardLight = mLightsService.getLight(LightsService.LIGHT_ID_KEYBOARD);
+            mCapsLight = mLightsService.getLight(LightsService.LIGHT_ID_CAPS);
+            mFnLight = mLightsService.getLight(LightsService.LIGHT_ID_FUNC);
 
             // Register for broadcasts from other components of the system.
             IntentFilter filter = new IntentFilter();
@@ -945,7 +949,24 @@ public final class PowerManagerService extends IPowerManager.Stub
             }
             if (mKeyboardVisible != visible) {
                 mKeyboardVisible = visible;
+                // If hiding keyboard, turn off leds
+                setKeyboardLight(false, 1);
+                setKeyboardLight(false, 2);
             }
+        }
+    }
+
+    public void setKeyboardLight(boolean on, int key) {
+        if (key == 1) {
+            if (on)
+                mCapsLight.setColor(0x00ffffff);
+            else
+                mCapsLight.turnOff();
+        } else if (key == 2) {
+            if (on)
+                mFnLight.setColor(0x00ffffff);
+            else
+                mFnLight.turnOff();
         }
     }
 