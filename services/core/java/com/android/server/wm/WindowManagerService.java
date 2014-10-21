64daef8b919bb7046f5ead6059a827f12402e257
tingna_sung <tingna_sung@htc.com>
2014-10-21 13:22:16 +0800

    Avoid capture all black screenshot bitmap
    Captured screenshot bitmap is all black one if none of windows is
    surface shown.
    
    Change-Id: I651b2ade1865926d61f945167d60219535fa8bf8
    Signed-off-by: tingna_sung <tingna_sung@htc.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -6244,7 +6244,7 @@ public class WindowManagerService extends IWindowManager.Stub
                     }
 
                     if (ws.mAppToken != null && ws.mAppToken.token == appToken &&
-                            ws.isDisplayedLw()) {
+                            ws.isDisplayedLw() && winAnim.mSurfaceShown) {
                         screenshotReady = true;
                     }
                 }
