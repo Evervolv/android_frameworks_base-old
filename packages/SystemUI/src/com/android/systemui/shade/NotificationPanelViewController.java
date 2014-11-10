93aa7aa436b944cb3c67a4e8f4e6ece5654d146d
Roman Birg <roman@cyngn.com>
2014-11-09 19:52:34 -0800

    SystemUI: Add quick settings pull down with one finger
    Logic is currently set to use 1/3 of right hand side of the statusbar to
    trigger a quick settings quick pull down, quickly.
    
    Change-Id: I3f536496f724f07d2419a5b2db5fa8f8261609e7
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: fix right hand side gestures on lockscreen
    
    two finger quick pulldown was not accounting for being in the keyguard
    adn was eating touch events when it shouldn't be.
    
    Change-Id: I625d48a4002aaaf2d55d7efaed65d892ab7aa13c
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: add Settings.System toggle for quick-quick settings pulldown
    
    Change-Id: I3c5dcfc23f39bf8be6c402e25f61e6f57a8add2c
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    From: Zhao Wei Liew <zhaoweiliew@gmail.com>
    SystemUI: statusbar: Fix QS quick pulldown feature
    
    Bring in the fixes from CM 13.
    
    This commit squashes the following patches from CM 13.0:
    68c40f32470d66e0f8973d33a4124a2181754fb0 SystemUI: fix QS quick pull down
    477f58dc1413943926f2b4428f5873c0bb04dab8 SystemUI: hook up settings observer in notificationpanel
    d2bec52c76a3bb2c9a7fa3533e40f782fa592826 SystemUI: Fix quick-quick settings pulldown settings
    eb10bc20db37978413362250c8546ae7ed865077 Add left QS quick pulldown (1/3)
    
    Change-Id: Ie122477261d96499c80640cf274d22fba6642a01
    
    From: Zhao Wei Liew <zhaoweiliew@gmail.com>
    (partial) SystemUI: Use Tuner API for CM settings
    Get rid of all the excess code by implementing TunerService.Tunable
    and observing any changes made to the settings through it.
    
    Remove UserContentObserver as the Tuner API handles user switches.
    
    Also remove some unused imports that were introduced in earlier
    CM commits, but were never removed since.
    
    [Pig]: Forward port to R
    
    Change-Id: I3c5dcfc23f39bf8be6c402e25f61e6f57a8add2c

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -211,6 +211,7 @@ import com.android.systemui.statusbar.policy.KeyguardUserSwitcherController;
 import com.android.systemui.statusbar.policy.KeyguardUserSwitcherView;
 import com.android.systemui.statusbar.policy.OnHeadsUpChangedListener;
 import com.android.systemui.statusbar.window.StatusBarWindowStateController;
+import com.android.systemui.tuner.TunerService;
 import com.android.systemui.unfold.SysUIUnfoldComponent;
 import com.android.systemui.util.Compile;
 import com.android.systemui.util.LargeScreenUtils;
@@ -218,6 +219,8 @@ import com.android.systemui.util.ListenerSet;
 import com.android.systemui.util.Utils;
 import com.android.systemui.util.time.SystemClock;
 import com.android.wm.shell.animation.FlingAnimationUtils;
+
+import evervolv.provider.EVSettings;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -304,6 +307,9 @@ public final class NotificationPanelViewController extends PanelViewController {
     private static final String COUNTER_PANEL_OPEN_QS = "panel_open_qs";
     private static final String COUNTER_PANEL_OPEN_PEEK = "panel_open_peek";
 
+    private static final String STATUS_BAR_QUICK_QS_PULLDOWN =
+            "evsystem:" + EVSettings.System.STATUS_BAR_QUICK_QS_PULLDOWN;
+
     private static final Rect M_DUMMY_DIRTY_RECT = new Rect(0, 0, 1, 1);
     private static final Rect EMPTY_RECT = new Rect();
 
@@ -345,6 +351,8 @@ public final class NotificationPanelViewController extends PanelViewController {
     private float mKeyguardNotificationTopPadding;
     /** Current max allowed keyguard notifications determined by measuring the panel. */
     private int mMaxAllowedKeyguardNotifications;
+
+    private final TunerService mTunerService;
 
     private KeyguardQsUserSwitchController mKeyguardQsUserSwitchController;
     private KeyguardUserSwitcherController mKeyguardUserSwitcherController;
@@ -663,6 +671,8 @@ public final class NotificationPanelViewController extends PanelViewController {
     private final Runnable mAnimateKeyguardBottomAreaInvisibleEndRunnable =
             () -> mKeyguardBottomArea.setVisibility(View.GONE);
 
+    private int mOneFingerQuickSettingsIntercept;
+
     private final AccessibilityDelegate mAccessibilityDelegate = new AccessibilityDelegate() {
         @Override
         public void onInitializeAccessibilityNodeInfo(View host,
@@ -777,7 +787,8 @@ public final class NotificationPanelViewController extends PanelViewController {
             SystemClock systemClock,
             CameraGestureHelper cameraGestureHelper,
             KeyguardBottomAreaViewModel keyguardBottomAreaViewModel,
-            KeyguardBottomAreaInteractor keyguardBottomAreaInteractor) {
+            KeyguardBottomAreaInteractor keyguardBottomAreaInteractor,
+            TunerService tunerService) {
         super(view,
                 falsingManager,
                 dozeLog,
@@ -823,6 +834,7 @@ public final class NotificationPanelViewController extends PanelViewController {
                 LargeScreenUtils.shouldUseSplitNotificationShade(mResources);
         mView.setWillNotDraw(!DEBUG_DRAWABLE);
         mLargeScreenShadeHeaderController = largeScreenShadeHeaderController;
+        mTunerService = tunerService;
         mLayoutInflater = layoutInflater;
         mFeatureFlags = featureFlags;
         mFalsingManager = falsingManager;
@@ -2178,7 +2190,22 @@ public final class NotificationPanelViewController extends PanelViewController {
                         MotionEvent.BUTTON_SECONDARY) || event.isButtonPressed(
                         MotionEvent.BUTTON_TERTIARY));
 
-        return twoFingerDrag || stylusButtonClickDrag || mouseButtonClickDrag;
+        final float w = mView.getMeasuredWidth();
+        final float x = event.getX();
+        float region = w * 1.f / 4.f; // TODO overlay region fraction?
+        boolean showQsOverride = false;
+
+        switch (mOneFingerQuickSettingsIntercept) {
+            case 1: // Right side pulldown
+                showQsOverride = mView.isLayoutRtl() ? x < region : w - region < x;
+                break;
+            case 2: // Left side pulldown
+                showQsOverride = mView.isLayoutRtl() ? w - region < x : x < region;
+                break;
+        }
+        showQsOverride &= mBarState == StatusBarState.SHADE;
+
+        return twoFingerDrag || showQsOverride || stylusButtonClickDrag || mouseButtonClickDrag;
     }
 
     private void handleQsDown(MotionEvent event) {
@@ -4779,7 +4806,8 @@ public final class NotificationPanelViewController extends PanelViewController {
         positionClockAndNotifications(true /* forceUpdate */);
     }
 
-    private class OnAttachStateChangeListener implements View.OnAttachStateChangeListener {
+    private class OnAttachStateChangeListener implements View.OnAttachStateChangeListener,
+            TunerService.Tunable {
         @Override
         public void onViewAttachedToWindow(View v) {
             mFragmentService.getFragmentHostManager(mView)
@@ -4787,6 +4815,7 @@ public final class NotificationPanelViewController extends PanelViewController {
             mStatusBarStateController.addCallback(mStatusBarStateListener);
             mStatusBarStateListener.onStateChanged(mStatusBarStateController.getState());
             mConfigurationController.addCallback(mConfigurationListener);
+            mTunerService.addTunable(this, STATUS_BAR_QUICK_QS_PULLDOWN);
             // Theme might have changed between inflating this view and attaching it to the
             // window, so
             // force a call to onThemeChanged
@@ -4803,7 +4832,15 @@ public final class NotificationPanelViewController extends PanelViewController {
                     .removeTagListener(QS.TAG, mFragmentListener);
             mStatusBarStateController.removeCallback(mStatusBarStateListener);
             mConfigurationController.removeCallback(mConfigurationListener);
+            mTunerService.removeTunable(this);
             mFalsingManager.removeTapListener(mFalsingTapListener);
+        }
+
+        @Override
+        public void onTuningChanged(String key, String newValue) {
+            if (STATUS_BAR_QUICK_QS_PULLDOWN.equals(key)) {
+                mOneFingerQuickSettingsIntercept = TunerService.parseInteger(newValue, 1);
+            }
         }
     }
 
