1970781a7ef0ec14cee668bfc65caceed9e98f8c
Steve Kondik <steve@cyngn.com>
2015-01-02 02:47:09 -0800

    perf: Boost during animations
     * Reduce animation jank by preemptively boostin the CPUs.
     * Original idea taken from CodeAurora, modified for CM perf control.
    
    Change-Id: Iff8f008117beb381c6667a268d1dd83d8c9e4cdf

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -1819,6 +1819,9 @@ final class ActivityStack {
                     mWindowManager.prepareAppTransition(prev.task == next.task
                             ? AppTransition.TRANSIT_ACTIVITY_CLOSE
                             : AppTransition.TRANSIT_TASK_CLOSE, false);
+                    if (prev.task != next.task) {
+                        mStackSupervisor.mPm.cpuBoost(2000 * 1000);
+                    }
                 }
                 mWindowManager.setAppWillBeHidden(prev.appToken);
                 mWindowManager.setAppVisibility(prev.appToken, false);
@@ -1834,6 +1837,9 @@ final class ActivityStack {
                             : next.mLaunchTaskBehind
                                     ? AppTransition.TRANSIT_TASK_OPEN_BEHIND
                                     : AppTransition.TRANSIT_TASK_OPEN, false);
+                    if (prev.task != next.task) {
+                        mStackSupervisor.mPm.cpuBoost(2000 * 1000);
+                    }
                 }
             }
             if (false) {
