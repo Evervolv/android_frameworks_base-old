6886cce1ea1d7b24c96f562bd9d4abdd6e90aa8c
Varad Deshmukh <varadd@codeaurora.org>
2015-03-26 19:39:19 -0700

    Modify GC Delay to include subsequent launches. (fix)
    Application launches for apps like GMS Music
    and Maps lead to explicit GC's due to Binder
    activity in the SystemServer. This change
    allows delaying the GC for 3 seconds after
    the application is launched. Subsequent launches
    or previously killed applications with existing
    activity records are also now covered.
    
    Change-Id: I1a915d5b8a8bf752991a429328fe398ff4f1eae2

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -2696,6 +2696,8 @@ public final class ActivityStackSupervisor implements DisplayListener {
 
     ActivityRecord findTaskLocked(ActivityRecord r) {
         if (DEBUG_TASKS) Slog.d(TAG, "Looking for task of " + r);
+        /* Delay Binder Explicit GC during application launch */
+        BinderInternal.modifyDelayedGcParams();
         for (int displayNdx = mActivityDisplays.size() - 1; displayNdx >= 0; --displayNdx) {
             final ArrayList<ActivityStack> stacks = mActivityDisplays.valueAt(displayNdx).mStacks;
             for (int stackNdx = stacks.size() - 1; stackNdx >= 0; --stackNdx) {
@@ -2716,9 +2718,6 @@ public final class ActivityStackSupervisor implements DisplayListener {
                 }
             }
         }
-        /* Delay Binder Explicit GC during application launch */
-        BinderInternal.modifyDelayedGcParams();
-
         mPm.cpuBoost(2000 * 1000);
         if (DEBUG_TASKS) Slog.d(TAG, "No task found");
         return null;
