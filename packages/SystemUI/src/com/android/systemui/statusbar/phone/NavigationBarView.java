51deea2133eb795734b4f08b3db16918bad44475
William Bellavance <flintman@flintmancomputers.com>
2016-02-29 19:01:57 -0500

    Revert "Vectorify system bar drawables"
    This reverts commit ff0be9b81563b8b992a28e17a5a5135a79b4d1e2.
    
    Change-Id: I643e399d7a7155378305df36844ffaa1eaa70fe0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -260,7 +260,7 @@ public class NavigationBarView extends LinearLayout {
 
     private void getIcons(Resources res) {
         mBackIcon = new BackButtonDrawable(res.getDrawable(R.drawable.ic_sysbar_back));
-        mBackLandIcon = new BackButtonDrawable(res.getDrawable(R.drawable.ic_sysbar_back_land));
+        mBackLandIcon = new BackButtonDrawable(res.getDrawable(R.drawable.ic_sysbar_back));
         mRecentIcon = res.getDrawable(R.drawable.ic_sysbar_recent);
         mRecentLandIcon = mRecentIcon;
         mHomeIcon = res.getDrawable(R.drawable.ic_sysbar_home);
