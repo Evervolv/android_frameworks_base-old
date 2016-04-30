4d7fa63df7796528cf9df31c13149af06c7dc768
Matadeen Mishra <matade@codeaurora.org>
2016-04-30 15:28:56 +0530

    BT: Set persiste Bluetooth State during enable timeout case
    Use case: When enable timeout occurs during S&S testing and
    Bluetooth state is moved from Turning On to Off.
    
    Steps: Changes related to enable timeout case, to test the
    changes, S&S testing can be done.
    
    Failure: Changes done to handle Bluetooth enable timeout
    case when Bluetooth state changes from Turning On to Off.
    
    Root cause: Setting Bluetooth Persist state to handle the
    enable timeout case so that Bluetooth doesnot self enable
    after enable timeout case is handled.
    
    Fix: Setting the Bluetooth Persist State to Off when enable
    timeout occurs.
    
    CRs-Fixed: 1006444
    Change-Id: I17b3b05020eb7469666e6d8c88c86df7764b4024

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -2035,6 +2035,8 @@ public final class BluetoothAdapter {
                 }
 
                 synchronized (mProxyServiceStateCallbacks) {
+                    Log.d(TAG, "onBluetoothServiceDown: Sending callbacks to " +
+                                    mProxyServiceStateCallbacks.size() + " clients");
                     for (IBluetoothManagerCallback cb : mProxyServiceStateCallbacks ){
                         try {
                             if (cb != null) {
@@ -2042,11 +2044,10 @@ public final class BluetoothAdapter {
                             } else {
                                 Log.d(TAG, "onBluetoothServiceDown: cb is null!!!");
                             }
-                        } catch (Exception e) {
-                            Log.e(TAG,"",e);
-                        }
+                        } catch (Exception e)  { Log.e(TAG,"",e);}
                     }
                 }
+                Log.d(TAG, "onBluetoothServiceDown: Finished sending callbacks to registered clients");
             }
 
             public void onBrEdrDown() {
