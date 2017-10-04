3cac2c19ac3a60305d192b519d76dc3e66448614
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-03 21:47:41 -0400

    SystemUI: Allow using tuner API for EVSettings
    Based on a squash of the following changes:
    
    Author: Steve Kondik <steve@cyngn.com>
    Date:   Tue Sep 13 23:46:34 2016 -0700
    
        systemui: Allow using tuner API for CMSettings
    
         * Use the prefix "cm:" on the key supplied and it will redirect
           to CMSettings.
    
        Change-Id: Id4266a568deb8fb32857136990711b5d9d487721
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Date:   Thu Oct 13 19:19:03 2016 +0800
    
        SystemUI: tuner: Allow Tuner API for System settings
    
        We'd like to modify some AOSP System settings as well.
    
        Change-Id: I4e2e7bf680f20bce9619ff2adf243d0f8f4a9906
    
    Change-Id: Ide9622ea4c7be371b83ff7ae9a73ce5565423af1
    
    TunerServiceImpl: Add support for Evervolv global settings
    
    Change-Id: I7f3a2e024286f2d7c49e173a7a0bb9a2f486c5d9
    
    TunerServiceImpl: Blacklist Evervolv settings from tuner reset
    
     * They use the tunable infrastructure but are real user settings and
       shouldn't be reset with tuner settings. Moreover, setting them to
       null would cause a IllegalArgumentException.
     * Simpified version of the original commit.
    
    Change-Id: Ie05910efaee867632a3bd2e84625d7735259130e

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -27,6 +27,7 @@ import android.net.Uri;
 import android.os.Handler;
 import android.os.HandlerExecutor;
 import android.os.Looper;
+import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
 import android.provider.Settings.Secure;
@@ -47,6 +48,8 @@ import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.phone.StatusBarIconController;
 import com.android.systemui.statusbar.phone.SystemUIDialog;
 import com.android.systemui.util.leak.LeakDetector;
+
+import evervolv.provider.EVSettings;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -162,31 +165,116 @@ public class TunerServiceImpl extends TunerService {
         setValue(TUNER_VERSION, newVersion);
     }
 
+    private boolean isEVGlobal(String key) {
+        return key.startsWith("evglobal:");
+    }
+
+    private boolean isEVSystem(String key) {
+        return key.startsWith("evsystem:");
+    }
+
+    private boolean isEVSecure(String key) {
+        return key.startsWith("evsecure:");
+    }
+
+    private boolean isSystem(String key) {
+        return key.startsWith("system:");
+    }
+
+    private String chomp(String key) {
+        return key.replaceFirst("^(evglobal|evsecure|evsystem|system):", "");
+    }
+
     @Override
     public String getValue(String setting) {
-        return Settings.Secure.getStringForUser(mContentResolver, setting, mCurrentUser);
+        if (isEVGlobal(setting)) {
+            return EVSettings.Global.getString(mContentResolver, chomp(setting));
+        } else if (isEVSecure(setting)) {
+            return EVSettings.Secure.getStringForUser(
+                    mContentResolver, chomp(setting), mCurrentUser);
+        } else if (isEVSystem(setting)) {
+            return EVSettings.System.getStringForUser(
+                    mContentResolver, chomp(setting), mCurrentUser);
+        } else if (isSystem(setting)) {
+            return Settings.System.getStringForUser(
+                    mContentResolver, chomp(setting), mCurrentUser);
+        } else {
+            return Settings.Secure.getStringForUser(mContentResolver, setting, mCurrentUser);
+        }
     }
 
     @Override
     public void setValue(String setting, String value) {
-         Settings.Secure.putStringForUser(mContentResolver, setting, value, mCurrentUser);
+        if (isEVGlobal(setting)) {
+            EVSettings.Global.putString(mContentResolver, chomp(setting), value);
+        } else if (isEVSecure(setting)) {
+            EVSettings.Secure.putStringForUser(
+                    mContentResolver, chomp(setting), value, mCurrentUser);
+        } else if (isEVSystem(setting)) {
+            EVSettings.System.putStringForUser(
+                    mContentResolver, chomp(setting), value, mCurrentUser);
+        } else if (isSystem(setting)) {
+            Settings.System.putStringForUser(
+                    mContentResolver, chomp(setting), value, mCurrentUser);
+        } else {
+            Settings.Secure.putStringForUser(mContentResolver, setting, value, mCurrentUser);
+        }
     }
 
     @Override
     public int getValue(String setting, int def) {
-        return Settings.Secure.getIntForUser(mContentResolver, setting, def, mCurrentUser);
+        if (isEVGlobal(setting)) {
+            return EVSettings.Global.getInt(mContentResolver, chomp(setting), def);
+        } else if (isEVSecure(setting)) {
+            return EVSettings.Secure.getIntForUser(
+                    mContentResolver, chomp(setting), def, mCurrentUser);
+        } else if (isEVSystem(setting)) {
+            return EVSettings.System.getIntForUser(
+                    mContentResolver, chomp(setting), def, mCurrentUser);
+        } else if (isSystem(setting)) {
+            return Settings.System.getIntForUser(
+                    mContentResolver, chomp(setting), def, mCurrentUser);
+        } else {
+            return Settings.Secure.getIntForUser(mContentResolver, setting, def, mCurrentUser);
+        }
     }
 
     @Override
     public String getValue(String setting, String def) {
-        String ret = Secure.getStringForUser(mContentResolver, setting, mCurrentUser);
+        String ret;
+        if (isEVGlobal(setting)) {
+            ret = EVSettings.Global.getString(mContentResolver, chomp(setting));
+        } else if (isEVSecure(setting)) {
+            ret = EVSettings.Secure.getStringForUser(
+                    mContentResolver, chomp(setting), mCurrentUser);
+        } else if (isEVSystem(setting)) {
+            ret = EVSettings.System.getStringForUser(
+                    mContentResolver, chomp(setting), mCurrentUser);
+        } else if (isSystem(setting)) {
+            ret = Settings.System.getStringForUser(
+                    mContentResolver, chomp(setting), mCurrentUser);
+        } else {
+            ret = Secure.getStringForUser(mContentResolver, setting, mCurrentUser);
+        }
         if (ret == null) return def;
         return ret;
     }
 
     @Override
     public void setValue(String setting, int value) {
-         Settings.Secure.putIntForUser(mContentResolver, setting, value, mCurrentUser);
+        if (isEVGlobal(setting)) {
+            EVSettings.Global.putInt(mContentResolver, chomp(setting), value);
+        } else if (isEVSecure(setting)) {
+            EVSettings.Secure.putIntForUser(
+                    mContentResolver, chomp(setting), value, mCurrentUser);
+        } else if (isEVSystem(setting)) {
+            EVSettings.System.putIntForUser(
+                    mContentResolver, chomp(setting), value, mCurrentUser);
+        } else if (isSystem(setting)) {
+            Settings.System.putIntForUser(mContentResolver, chomp(setting), value, mCurrentUser);
+        } else {
+            Settings.Secure.putIntForUser(mContentResolver, setting, value, mCurrentUser);
+        }
     }
 
     @Override
@@ -205,14 +293,25 @@ public class TunerServiceImpl extends TunerService {
             mTunables.add(tunable);
             mLeakDetector.trackCollection(mTunables, "TunerService.mTunables");
         }
-        Uri uri = Settings.Secure.getUriFor(key);
+        final Uri uri;
+        if (isEVGlobal(key)) {
+            uri = EVSettings.Global.getUriFor(chomp(key));
+        } else if (isEVSecure(key)) {
+            uri = EVSettings.Secure.getUriFor(chomp(key));
+        } else if (isEVSystem(key)) {
+            uri = EVSettings.System.getUriFor(chomp(key));
+        } else if (isSystem(key)) {
+            uri = Settings.System.getUriFor(chomp(key));
+        } else {
+            uri = Settings.Secure.getUriFor(key);
+        }
         if (!mListeningUris.containsKey(uri)) {
             mListeningUris.put(uri, key);
-            mContentResolver.registerContentObserver(uri, false, mObserver, mCurrentUser);
+            mContentResolver.registerContentObserver(uri, false, mObserver,
+                    isEVGlobal(key) ? UserHandle.USER_ALL : mCurrentUser);
         }
         // Send the first state.
-        String value = DejankUtils.whitelistIpcs(() -> Settings.Secure
-                .getStringForUser(mContentResolver, key, mCurrentUser));
+        String value = DejankUtils.whitelistIpcs(() -> getValue(key));
         tunable.onTuningChanged(key, value);
     }
 
@@ -232,7 +331,9 @@ public class TunerServiceImpl extends TunerService {
         }
         mContentResolver.unregisterContentObserver(mObserver);
         for (Uri uri : mListeningUris.keySet()) {
-            mContentResolver.registerContentObserver(uri, false, mObserver, mCurrentUser);
+            String key = mListeningUris.get(uri);
+            mContentResolver.registerContentObserver(uri, false, mObserver,
+                    isEVGlobal(key) ? UserHandle.USER_ALL : mCurrentUser);
         }
     }
 
@@ -242,7 +343,7 @@ public class TunerServiceImpl extends TunerService {
         if (tunables == null) {
             return;
         }
-        String value = Settings.Secure.getStringForUser(mContentResolver, key, mCurrentUser);
+        String value = getValue(key);
         for (Tunable tunable : tunables) {
             tunable.onTuningChanged(key, value);
         }
@@ -250,8 +351,7 @@ public class TunerServiceImpl extends TunerService {
 
     private void reloadAll() {
         for (String key : mTunableLookup.keySet()) {
-            String value = Settings.Secure.getStringForUser(mContentResolver, key,
-                    mCurrentUser);
+            String value = getValue(key);
             for (Tunable tunable : mTunableLookup.get(key)) {
                 tunable.onTuningChanged(key, value);
             }
@@ -270,10 +370,11 @@ public class TunerServiceImpl extends TunerService {
 
         // A couple special cases.
         for (String key : mTunableLookup.keySet()) {
-            if (ArrayUtils.contains(RESET_EXCEPTION_LIST, key)) {
+            if (ArrayUtils.contains(RESET_EXCEPTION_LIST, key)
+                    || isEVGlobal(key) || isEVSecure(key) || isEVSystem(key)) {
                 continue;
             }
-            Settings.Secure.putStringForUser(mContentResolver, key, null, user);
+            setValue(key, null);
         }
     }
 
@@ -326,8 +427,9 @@ public class TunerServiceImpl extends TunerService {
         @Override
         public void onChange(boolean selfChange, java.util.Collection<Uri> uris,
                 int flags, int userId) {
-            if (userId == mUserTracker.getUserId()) {
-                for (Uri u : uris) {
+            for (Uri u : uris) {
+                String key = mListeningUris.get(u);
+                if (userId == mUserTracker.getUserId() || isEVGlobal(key)) {
                     reloadSetting(u);
                 }
             }
