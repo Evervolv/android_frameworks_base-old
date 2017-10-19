2a58e821aa7d5d6234cc7cfc52e3de423897e211
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-18 20:33:08 -0400

    SystemUI: Bringup tuner navbar and statusbar changes
    Based on the following changes:
    
    Author: Steve Kondik <steve@cyngn.com>
    Date:   Mon Sep 12 12:49:59 2016 -0700
    
        systemui: Enable navbar tuner
    
         * And add an action for it.
    
        Change-Id: I23a51cc8e33f3498deec9ed60281423c66bbc4ca
    
    Author: Steve Kondik <steve@cyngn.com>
    Date:   Wed Sep 14 05:08:02 2016 -0700
    
        systemui: Fix a few issues with Tuner
    
         * Allow launching PreferenceScreens directly by setting the
           "tuner" extra to a valid key.
         * Remove the silly warning. There are no dragons here.
         * Move CM stuff to CM manifest.
         * Fix the ActionBar title when navigating.
    
        Change-Id: Ideac31dbfd71d7c0aa7fc3c20395c24128c598fc
    
    Author: Joey Rizzoli <joey@cyanogenmoditalia.it>
    Date:   Tue Nov 8 15:44:25 2016 +0100
    
        Tuner: allow power notif controls to be launched externally
    
        Change-Id: I8a9e1a2080640938c99125ab04986dfc99e004bf
        Signed-off-by: Joey Rizzoli <joey@cyanogenmoditalia.it>
    
    Author: Gabriele M <moto.falcon.git@gmail.com>
    Date:   Sat Sep 16 19:35:28 2017 +0200
    
        SystemUI: Revert some Tuner changes
    
        These changes were done to start the status bar tuner directly without
        showing the main Tuner screen. However, they negatively affect the
        management of the fragments. Revert them so that a different solution
        can be implemented in a follow-up change.
    
        This reverts the following commits:
    
         - 338de6728d8910d450f8962a44bbe69feba1ced4
           "Handle onOptionsItemSelected for TunerActivity"
    
         - b3eba6af8e3253efc1332b55821fd08e4fbf77b0
           "SystemUI: Remove nav bar and status bar options from SystemUI Tuner"
    
         - 82b9f95f761e7b5cc94ea2542b30460e48f8fb02 (partially)
           "systemui: Fix a few issues with Tuner"
    
        BUGBASH-661
    
        Change-Id: I35182248566ff31ed08d2836d66d5bd3f947ee9a
    
    Author: Gabriele M <moto.falcon.git@gmail.com>
    Date:   Sat Sep 16 19:35:38 2017 +0200
    
        SystemUI: Add separate pref screen for the status bar tuner
    
        Keep the status bar preferences separate and allow to start them
        directly without loading the main tuner fragment.
    
        Change-Id: Ie1a0d9395b7bc01d585616a1976adeda3c457a0d
    
    Change-Id: Icc84575db18931d3a43bd9e640f2a55a51b460c1

--- packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
@@ -15,13 +15,8 @@
  */
 package com.android.systemui.tuner;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v14.preference.PreferenceFragment;
 import android.support.v7.preference.Preference;
 import android.view.Menu;
@@ -42,9 +37,6 @@ public class TunerFragment extends PreferenceFragment {
     private static final String KEY_PLUGINS = "plugins";
     private static final CharSequence KEY_DOZE = "doze";
 
-    public static final String SETTING_SEEN_TUNER_WARNING = "seen_tuner_warning";
-
-    private static final String WARNING_TAG = "tuner_warning";
     private static final String[] DEBUG_ONLY = new String[] {
             "nav_bar",
             "lockscreen",
@@ -79,13 +71,6 @@ public class TunerFragment extends PreferenceFragment {
             for (int i = 0; i < DEBUG_ONLY.length; i++) {
                 Preference preference = findPreference(DEBUG_ONLY[i]);
                 if (preference != null) getPreferenceScreen().removePreference(preference);
-            }
-        }
-
-        if (Settings.Secure.getInt(getContext().getContentResolver(), SETTING_SEEN_TUNER_WARNING,
-                0) == 0) {
-            if (getFragmentManager().findFragmentByTag(WARNING_TAG) == null) {
-                new TunerWarningFragment().show(getFragmentManager(), WARNING_TAG);
             }
         }
     }
@@ -132,21 +117,5 @@ public class TunerFragment extends PreferenceFragment {
                 return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    public static class TunerWarningFragment extends DialogFragment {
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            return new AlertDialog.Builder(getContext())
-                    .setTitle(R.string.tuner_warning_title)
-                    .setMessage(R.string.tuner_warning)
-                    .setPositiveButton(R.string.got_it, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Settings.Secure.putInt(getContext().getContentResolver(),
-                                    SETTING_SEEN_TUNER_WARNING, 1);
-                        }
-                    }).show();
-        }
     }
 }
