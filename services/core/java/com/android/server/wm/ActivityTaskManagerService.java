9c07a4559ffa19c2266d7dfa698314180eb7a3e8
Jesse Chan <jc@lineageos.org>
2018-04-21 23:45:57 -0700

    Add an option to force pre-O apps to use full screen aspect ratio
    When an app target pre-O releases, the default max aspect ratio
    is 1.86:1 which leads to ugly black areas on devices that have
    screens with higher aspect ratio (for example Galaxy S8/S9).
    
    This change adds an option to allow users to change aspect ratio
    for pre-O apps to full screen aspect ratio.
    
    Change-Id: I41d5408841593a12443be885e11959bffaebb67b

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -289,6 +289,8 @@ import com.android.server.uri.UriGrantsManagerInternal;
 import com.android.server.wallpaper.WallpaperManagerInternal;
 import com.android.wm.shell.Flags;
 
+import com.evervolv.internal.applications.ActivityManagerExt;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
@@ -803,6 +805,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     private Set<Integer> mProfileOwnerUids = new ArraySet<Integer>();
 
+    // Activity related helper
+    private ActivityManagerExt mActivityManagerExt;
+
     private final class SettingObserver extends ContentObserver {
         private final Uri mFontScaleUri = Settings.System.getUriFor(FONT_SCALE);
         private final Uri mHideErrorDialogsUri = Settings.Global.getUriFor(HIDE_ERROR_DIALOGS);
@@ -902,6 +907,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     public void installSystemProviders() {
         mSettingsObserver = new SettingObserver();
+        // ActivityManagerExt depends on settings so we can initialize only
+        // after providers are available.
+        mActivityManagerExt = new ActivityManagerExt(mContext);
     }
 
     public void retrieveSettings(ContentResolver resolver) {
@@ -7362,5 +7370,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     static boolean isPip2ExperimentEnabled() {
         return Flags.enablePip2Implementation();
+    }
+
+    public boolean shouldForceLongScreen(String packageName) {
+        return mActivityManagerExt.shouldForceLongScreen(packageName);
     }
 }
