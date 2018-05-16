1e5053059ebb00e8c1f83848f556d54cc1bf81b6
Joey <joey@lineageos.org>
2018-05-16 17:16:06 +0200

    SystemUI: Add reading mode tile
    Author: Joey <joey@lineageos.org>
    Date:   Wed May 16 17:16:06 2018 +0200
    
        base: add reading mode tile
    
        Change-Id: I1c515cbeff0dd5a1fa0c00bc9390a66eb104c7f0
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sat May 26 00:58:57 2018 +0300
    
        SystemUI: Fix reading mode tile accessibility strings
    
        Change-Id: I4f0af56e4947b037e1e2dda79132523d13458818
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Wed Aug 8 18:59:25 2018 +0200
    
        SystemUI: Update reading mode tile for new API
    
        Change-Id: I9fbf1b4de30079c987b75e37e52e234fa2f86c5d
    
    Change-Id: I365ee54b2b2adc52d804a5fc1d35690ef5636071

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -50,6 +50,7 @@ import com.android.systemui.qs.tiles.OneHandedModeTile;
 import com.android.systemui.qs.tiles.PowerShareTile;
 import com.android.systemui.qs.tiles.QRCodeScannerTile;
 import com.android.systemui.qs.tiles.QuickAccessWalletTile;
+import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.ReduceBrightColorsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
@@ -98,6 +99,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<QRCodeScannerTile> mQRCodeScannerTileProvider;
     private final Provider<OneHandedModeTile> mOneHandedModeTileProvider;
     private final Provider<PowerShareTile> mPowerShareTileProvider;
+    private final Provider<ReadingModeTile> mReadingModeTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -135,7 +137,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<QRCodeScannerTile> qrCodeScannerTileProvider,
             Provider<OneHandedModeTile> oneHandedModeTileProvider,
             Provider<ColorCorrectionTile> colorCorrectionTileProvider,
-            Provider<PowerShareTile> powerShareTileProvider) {
+            Provider<PowerShareTile> powerShareTileProvider,
+            Provider<ReadingModeTile> readingModeTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -169,6 +172,7 @@ public class QSFactoryImpl implements QSFactory {
         mOneHandedModeTileProvider = oneHandedModeTileProvider;
         mColorCorrectionTileProvider = colorCorrectionTileProvider;
         mPowerShareTileProvider = powerShareTileProvider;
+        mReadingModeTileProvider = readingModeTileProvider;
     }
 
     /** Creates a tile with a type based on {@code tileSpec} */
@@ -244,6 +248,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mColorCorrectionTileProvider.get();
             case "powershare":
                 return mPowerShareTileProvider.get();
+            case "reading_mode":
+                return mReadingModeTileProvider.get();
         }
 
         // Custom tiles
