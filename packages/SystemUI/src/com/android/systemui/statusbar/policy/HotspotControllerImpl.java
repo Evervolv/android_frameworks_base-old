b9ac348cfb27e485e0c11126836706833a780221
Abhishek Srivastava <cabsriv@codeaurora.org>
2018-07-09 19:05:28 +0530

    Wifi: Check for WiFiService's existence before its access
    SystemUI , currently accesses Wi-FiService's API's before it is initialized/
    started.The current behaviour is to not start the Wi-Fi Service during the
    FDE encryption ( Wi-FI Service is not considered as a coreService). This
    behavior would lead to a fatal exception. Address this by adding a NULL check
    before the access in all the components of systemUI.
    
    Change-Id: I5fb753df8874b962622ef6921b9cbc518381203c
    CRs-Fixed: 2276734

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/HotspotControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/HotspotControllerImpl.java
@@ -109,12 +109,14 @@ public class HotspotControllerImpl implements HotspotController, WifiManager.Sof
      * @param shouldListen whether we should start listening to various wifi statuses
      */
     private void updateWifiStateListeners(boolean shouldListen) {
-        if (shouldListen) {
-            mWifiManager.registerSoftApCallback(
-                    this,
-                    Dependency.get(Dependency.MAIN_HANDLER));
-        } else {
-            mWifiManager.unregisterSoftApCallback(this);
+        if (mWifiManager != null) {
+            if (shouldListen) {
+                mWifiManager.registerSoftApCallback(
+                        this,
+                        Dependency.get(Dependency.MAIN_HANDLER));
+            } else {
+                mWifiManager.unregisterSoftApCallback(this);
+            }
         }
     }
 
