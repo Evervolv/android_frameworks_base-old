LineageOS/android_frameworks_base

perf: Support AIDL Lineage power HAL service
 * Call the new AIDL HAL where available and fall back to the HIDL HAL
   if not.
 * Map existing HIDL hint call to AIDL HAL.

TODO: Invoke the APIs directly from lineage sdk once killed HIDL.

Change-Id: Ieffa8f5c13c59b901cb030b7bb8a190793cd8cee

services/core/jni/com_android_server_power_PowerManagerService.cpp
@@ -26,6 +26,7 @@
 #include <android/system/suspend/ISuspendControlService.h>
 #include <nativehelper/JNIHelp.h>
 #include <vendor/lineage/power/1.0/ILineagePower.h>
+#include <vendor/lineage/power/IPower.h>
 #include "jni.h"
 
 #include <nativehelper/ScopedUtfChars.h>
@@ -63,7 +64,11 @@ using IPowerV1_1 = android::hardware::power::V1_1::IPower;
 using IPowerV1_0 = android::hardware::power::V1_0::IPower;
 using IPowerAidl = android::hardware::power::IPower;
 using ILineagePowerV1_0 = vendor::lineage::power::V1_0::ILineagePower;
-using vendor::lineage::power::V1_0::LineageFeature;
+using ILineagePowerAidl = vendor::lineage::power::IPower;
+using LineageBoostAidl = vendor::lineage::power::Boost;
+using LineageFeatureV1_0 = vendor::lineage::power::V1_0::LineageFeature;
+using LineageFeatureAidl = vendor::lineage::power::Feature;
+using LineagePowerHint1_0 = vendor::lineage::power::V1_0::LineagePowerHint;
 
 namespace android {
 
@@ -80,7 +85,9 @@ static sp<IPowerV1_0> gPowerHalHidlV1_0_ = nullptr;
 static sp<IPowerV1_1> gPowerHalHidlV1_1_ = nullptr;
 static sp<IPowerAidl> gPowerHalAidl_ = nullptr;
 static sp<ILineagePowerV1_0> gLineagePowerHalV1_0_ = nullptr;
+static sp<ILineagePowerAidl> gLineagePowerHalAidl_ = nullptr;
 static std::mutex gPowerHalMutex;
+static std::mutex gLineagePowerHalMutex;
 
 enum class HalVersion {
     NONE,
@@ -148,19 +155,39 @@ static HalVersion connectPowerHalLocked() {
     return HalVersion::NONE;
 }
 
-// Check validity of current handle to the Lineage power HAL service, and call getService() if necessary.
-// The caller must be holding gPowerHalMutex.
-void connectLineagePowerHalLocked() {
-    static bool gLineagePowerHalExists = true;
-    if (gLineagePowerHalExists && gLineagePowerHalV1_0_ == nullptr) {
-        gLineagePowerHalV1_0_ = ILineagePowerV1_0::getService();
-        if (gLineagePowerHalV1_0_ != nullptr) {
-            ALOGI("Loaded power HAL service");
+// Check validity of current handle to the Lineage power HAL service, and connect to it if necessary.
+// The caller must be holding gLineagePowerHalMutex.
+static HalVersion connectLineagePowerHalLocked() {
+    static bool gPowerHalHidlExists = true;
+    static bool gPowerHalAidlExists = true;
+    if (!gPowerHalHidlExists && !gPowerHalAidlExists) {
+        return HalVersion::NONE;
+    }
+    if (gPowerHalAidlExists) {
+        if (!gLineagePowerHalAidl_) {
+            gLineagePowerHalAidl_ = waitForVintfService<ILineagePowerAidl>();
+        }
+        if (gLineagePowerHalAidl_) {
+            ALOGV("Successfully connected to Lineage Power HAL AIDL service.");
+            return HalVersion::AIDL;
         } else {
-            ALOGI("Couldn't load power HAL service");
-            gLineagePowerHalExists = false;
+            gPowerHalAidlExists = false;
         }
     }
+    if (gPowerHalHidlExists && gLineagePowerHalV1_0_ == nullptr) {
+        gLineagePowerHalV1_0_ = ILineagePowerV1_0::getService();
+        if (gLineagePowerHalV1_0_) {
+            ALOGV("Successfully connected to Lineage Power HAL HIDL 1.0 service.");
+        } else {
+            ALOGV("Couldn't load Lineage power HAL HIDL service");
+            gPowerHalHidlExists = false;
+            return HalVersion::NONE;
+        }
+    }
+    if (gLineagePowerHalV1_0_) {
+        return HalVersion::HIDL_1_0;
+    }
+    return HalVersion::NONE;
 }
 
 // Retrieve a copy of PowerHAL HIDL V1_0
@@ -184,11 +211,14 @@ sp<IPowerV1_1> getPowerHalHidlV1_1() {
     return nullptr;
 }
 
-// Retrieve a copy of LineagePowerHAL V1_0
-sp<ILineagePowerV1_0> getLineagePowerHalV1_0() {
-    std::lock_guard<std::mutex> lock(gPowerHalMutex);
-    connectLineagePowerHalLocked();
-    return gLineagePowerHalV1_0_;
+// Retrieve a copy of LineagePowerHAL AIDL
+sp<ILineagePowerAidl> getLineagePowerHalAidl() {
+    std::lock_guard<std::mutex> lock(gLineagePowerHalMutex);
+    if (connectLineagePowerHalLocked() == HalVersion::AIDL) {
+        return gLineagePowerHalAidl_;
+    }
+
+    return nullptr;
 }
 
 // Check if a call to a power HAL function failed; if so, log the failure and invalidate the
@@ -338,6 +368,17 @@ static void sendPowerHint(PowerHint hintId, uint32_t data) {
                 lock.unlock();
                 setPowerModeWithHandle(handle, Mode::VR, static_cast<bool>(data));
                 break;
+            // TODO: Fix lineage sdk once killed hidl.
+            } else if (hintId == static_cast<PowerHint>(LineagePowerHint1_0::CPU_BOOST)) {
+                lock.unlock();
+                sp<ILineagePowerAidl> handle = getLineagePowerHalAidl();
+                handle->setBoost(LineageBoostAidl::CPU_BOOST, static_cast<bool>(data));
+                break;
+            } else if (hintId == static_cast<PowerHint>(LineagePowerHint1_0::SET_PROFILE)) {
+                lock.unlock();
+                sp<ILineagePowerAidl> handle = getLineagePowerHalAidl();
+                handle->setBoost(LineageBoostAidl::SET_PROFILE, static_cast<bool>(data));
+                break;
             } else {
                 ALOGE("Unsupported power hint: %s.", toString(hintId).c_str());
                 return;
@@ -439,12 +480,34 @@ void disableAutoSuspend() {
 
 static jint nativeGetFeature(JNIEnv* /* env */, jclass /* clazz */, jint featureId) {
     int value = -1;
-
-    sp<ILineagePowerV1_0> lineagePowerHalV1_0 = getLineagePowerHalV1_0();
-    if (lineagePowerHalV1_0 != nullptr) {
-        value = lineagePowerHalV1_0->getFeature(static_cast<LineageFeature>(featureId));
+    std::unique_lock<std::mutex> lock(gLineagePowerHalMutex);
+    switch (connectLineagePowerHalLocked()) {
+        case HalVersion::NONE:
+            break;
+        case HalVersion::HIDL_1_0: {
+            sp<ILineagePowerV1_0> handle = gLineagePowerHalV1_0_;
+            lock.unlock();
+            value = handle->getFeature(static_cast<LineageFeatureV1_0>(featureId));
+            break;
+        }
+        case HalVersion::AIDL: {
+            sp<ILineagePowerAidl> handle = gLineagePowerHalAidl_;
+            lock.unlock();
+            switch (static_cast<LineageFeatureV1_0>(featureId)) {
+                case LineageFeatureV1_0::SUPPORTED_PROFILES:
+                    handle->getFeature(LineageFeatureAidl::SUPPORTED_PROFILES, &value);
+                    break;
+                default:
+                    ALOGE("Unsupported power feature: %d.", featureId);
+                    break;
+            }
+            break;
+        }
+        default: {
+            ALOGE("Unknown Lineage power HAL state");
+            break;
+        }
     }
-
     return static_cast<jint>(value);
 }
 

Evervolv/android_frameworks_base-old

services/core/jni/com_android_server_power_PowerManagerService.cpp
@@ -26,6 +26,7 @@
 #include <android/system/suspend/ISuspendControlService.h>
 #include <nativehelper/JNIHelp.h>
 #include <vendor/evervolv/power/1.0/IEvervolvPower.h>
+#include <vendor/evervolv/power/IPower.h>
 #include "jni.h"
 
 #include <nativehelper/ScopedUtfChars.h>
@@ -62,7 +63,11 @@ using IPowerV1_1 = android::hardware::power::V1_1::IPower;
 using IPowerV1_0 = android::hardware::power::V1_0::IPower;
 using IPowerAidl = android::hardware::power::IPower;
 using IEvervolvPowerV1_0 = vendor::evervolv::power::V1_0::IEvervolvPower;
-using vendor::evervolv::power::V1_0::EvervolvFeature;
+using IEvervolvPowerAidl = vendor::evervolv::power::IPower;
+using EvervolvBoostAidl = vendor::evervolv::power::Boost;
+using EvervolvFeatureV1_0 = vendor::evervolv::power::V1_0::EvervolvFeature;
+using EvervolvFeatureAidl = vendor::evervolv::power::Feature;
+using EvervolvPowerHint1_0 = vendor::evervolv::power::V1_0::EvervolvPowerHint;
 
 namespace android {
 
@@ -79,8 +84,9 @@ static sp<IPowerV1_0> gPowerHalHidlV1_0_ = nullptr;
 static sp<IPowerV1_1> gPowerHalHidlV1_1_ = nullptr;
 static sp<IPowerAidl> gPowerHalAidl_ = nullptr;
 static sp<IEvervolvPowerV1_0> gEvervolvPowerHalV1_0_ = nullptr;
-static bool gEvervolvPowerHalExists = true;
+static sp<IEvervolvPowerAidl> gEvervolvPowerHalAidl_ = nullptr;
 static std::mutex gPowerHalMutex;
+static std::mutex gEvervolvPowerHalMutex;
 
 enum class HalVersion {
     NONE,
@@ -148,18 +154,39 @@ static HalVersion connectPowerHalLocked() {
     return HalVersion::NONE;
 }
 
-// Check validity of current handle to the Evervolv power HAL service, and call getService() if necessary.
-// The caller must be holding gPowerHalMutex.
-void connectEvervolvPowerHalLocked() {
-    if (gEvervolvPowerHalExists && gEvervolvPowerHalV1_0_ == nullptr) {
-        gEvervolvPowerHalV1_0_ = IEvervolvPowerV1_0::getService();
-        if (gEvervolvPowerHalV1_0_ != nullptr) {
-            ALOGI("Loaded power HAL service");
+// Check validity of current handle to the Evervolv power HAL service, and connect to it if necessary.
+// The caller must be holding gEvervolvPowerHalMutex.
+static HalVersion connectEvervolvPowerHalLocked() {
+    static bool gPowerHalHidlExists = true;
+    static bool gPowerHalAidlExists = true;
+    if (!gPowerHalHidlExists && !gPowerHalAidlExists) {
+        return HalVersion::NONE;
+    }
+    if (gPowerHalAidlExists) {
+        if (!gEvervolvPowerHalAidl_) {
+            gEvervolvPowerHalAidl_ = waitForVintfService<IEvervolvPowerAidl>();
+        }
+        if (gEvervolvPowerHalAidl_) {
+            ALOGV("Successfully connected to Evervolv Power HAL AIDL service.");
+            return HalVersion::AIDL;
         } else {
-            ALOGI("Couldn't load power HAL service");
-            gEvervolvPowerHalExists = false;
+            gPowerHalAidlExists = false;
         }
     }
+    if (gPowerHalHidlExists && gEvervolvPowerHalV1_0_ == nullptr) {
+        gEvervolvPowerHalV1_0_ = IEvervolvPowerV1_0::getService();
+        if (gEvervolvPowerHalV1_0_) {
+            ALOGV("Successfully connected to Evervolv Power HAL HIDL 1.0 service.");
+        } else {
+            ALOGV("Couldn't load Evervolv power HAL HIDL service");
+            gPowerHalHidlExists = false;
+            return HalVersion::NONE;
+        }
+    }
+    if (gEvervolvPowerHalV1_0_) {
+        return HalVersion::HIDL_1_0;
+    }
+    return HalVersion::NONE;
 }
 
 // Retrieve a copy of PowerHAL HIDL V1_0
@@ -183,11 +210,14 @@ sp<IPowerV1_1> getPowerHalHidlV1_1() {
     return nullptr;
 }
 
-// Retrieve a copy of EvervolvPowerHAL V1_0
-sp<IEvervolvPowerV1_0> getEvervolvPowerHalV1_0() {
-    std::lock_guard<std::mutex> lock(gPowerHalMutex);
-    connectEvervolvPowerHalLocked();
-    return gEvervolvPowerHalV1_0_;
+// Retrieve a copy of EvervolvPowerHAL AIDL
+sp<IEvervolvPowerAidl> getEvervolvPowerHalAidl() {
+    std::lock_guard<std::mutex> lock(gEvervolvPowerHalMutex);
+    if (connectEvervolvPowerHalLocked() == HalVersion::AIDL) {
+        return gEvervolvPowerHalAidl_;
+    }
+
+    return nullptr;
 }
 
 // Check if a call to a power HAL function failed; if so, log the failure and invalidate the
@@ -334,6 +364,17 @@ static void sendPowerHint(PowerHint hintId, uint32_t data) {
                 sp<IPowerAidl> handle = gPowerHalAidl_;
                 lock.unlock();
                 setPowerModeWithHandle(handle, Mode::VR, static_cast<bool>(data));
+                break;
+            // TODO: Fix evervolv sdk once killed hidl.
+            } else if (hintId == static_cast<PowerHint>(EvervolvPowerHint1_0::CPU_BOOST)) {
+                lock.unlock();
+                sp<IEvervolvPowerAidl> handle = getEvervolvPowerHalAidl();
+                handle->setBoost(EvervolvBoostAidl::CPU_BOOST, data);
+                break;
+            } else if (hintId == static_cast<PowerHint>(EvervolvPowerHint1_0::SET_PROFILE)) {
+                lock.unlock();
+                sp<IEvervolvPowerAidl> handle = getEvervolvPowerHalAidl();
+                handle->setBoost(EvervolvBoostAidl::SET_PROFILE, data);
                 break;
             } else {
                 ALOGE("Unsupported power hint: %s.", toString(hintId).c_str());
@@ -511,7 +552,7 @@ static jboolean nativeSetPowerMode(JNIEnv* /* env */, jclass /* clazz */, jint m
 }
 
 static void nativeSetFeature(JNIEnv* /* env */, jclass /* clazz */, jint featureId, jint data) {
-    std::unique_lock<std::mutex> lock(gPowerHalMutex);
+    std::unique_lock<std::mutex> lock(gEvervolvPowerHalMutex);
     switch (connectPowerHalLocked()) {
         case HalVersion::NONE:
             return;
@@ -545,12 +586,34 @@ static bool nativeForceSuspend(JNIEnv* /* env */, jclass /* clazz */) {
 
 static jint nativeGetFeature(JNIEnv* /* env */, jclass /* clazz */, jint featureId) {
     int value = -1;
-
-    sp<IEvervolvPowerV1_0> evervolvPowerHalV1_0 = getEvervolvPowerHalV1_0();
-    if (evervolvPowerHalV1_0 != nullptr) {
-        value = evervolvPowerHalV1_0->getFeature(static_cast<EvervolvFeature>(featureId));
+    std::unique_lock<std::mutex> lock(gPowerHalMutex);
+    switch (connectEvervolvPowerHalLocked()) {
+        case HalVersion::NONE:
+            break;
+        case HalVersion::HIDL_1_0: {
+            sp<IEvervolvPowerV1_0> handle = gEvervolvPowerHalV1_0_;
+            lock.unlock();
+            value = handle->getFeature(static_cast<EvervolvFeatureV1_0>(featureId));
+            break;
+        }
+        case HalVersion::AIDL: {
+            sp<IEvervolvPowerAidl> handle = gEvervolvPowerHalAidl_;
+            lock.unlock();
+            switch (static_cast<EvervolvFeatureV1_0>(featureId)) {
+                case EvervolvFeatureV1_0::SUPPORTED_PROFILES:
+                    handle->getFeature(EvervolvFeatureAidl::SUPPORTED_PROFILES, &value);
+                    break;
+                default:
+                    ALOGE("Unsupported power feature: %d.", featureId);
+                    break;
+            }
+            break;
+        }
+        default: {
+            ALOGE("Unknown Evervolv power HAL state");
+            break;
+        }
     }
-
     return static_cast<jint>(value);
 }
 