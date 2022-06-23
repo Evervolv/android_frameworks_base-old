LineageOS/android_frameworks_base

fixup! Allow lineage resources package to be overlayed by RRO packages
Change-Id: I7b1a53191ad613741159820bdd17bcb56d9872ef

services/core/java/com/android/server/om/OverlayManagerSettings.java
@@ -235,8 +235,8 @@ final class OverlayManagerSettings {
     }
 
     private static boolean isImmutableFrameworkOverlay(@NonNull SettingsItem item) {
-        return !item.isMutable() && "android".equals(item.getTargetPackageName())
-               && "lineageos.platform".equals(item.getTargetPackageName());
+        return !item.isMutable() && ("android".equals(item.getTargetPackageName())
+               || "lineageos.platform".equals(item.getTargetPackageName()));
     }
 
     /**

Evervolv/android_frameworks_base-old

services/core/java/com/android/server/om/OverlayManagerSettings.java
@@ -235,8 +235,8 @@ final class OverlayManagerSettings {
     }
 
     private static boolean isImmutableFrameworkOverlay(@NonNull SettingsItem item) {
-        return !item.isMutable() && "android".equals(item.getTargetPackageName())
-               && "evervolv.platform".equals(item.getTargetPackageName());
+        return !item.isMutable() && ("android".equals(item.getTargetPackageName())
+               || "evervolv.platform".equals(item.getTargetPackageName()));
     }
 
     /**