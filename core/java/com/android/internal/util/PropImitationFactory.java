fc26c836bc044bbd5d305f79584fef08122812f4
Nick Reuter <nreuter85@gmail.com>
2022-07-31 23:22:20 -0500

    PropImitationFactory: cleanup and add device filter
    Change-Id: Ic16945732c37b710b6678ed9754a37e9a3d74d3f

--- core/java/com/android/internal/util/PropImitationFactory.java
+++ core/java/com/android/internal/util/PropImitationFactory.java
@@ -20,6 +20,7 @@ package com.android.internal.util;
 import android.app.Application;
 import android.content.Context;
 import android.os.Build;
+import android.os.SystemProperties;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -27,7 +28,9 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import com.android.internal.R;
 
@@ -38,22 +41,30 @@ public class PropImitationFactory {
 
     private static volatile boolean mIsCallerPackageGms;
 
+    public static final String USER_PACKAGE_LIST = "vendor.property.imitation.packagelist";
+
     public static final String PACKAGE_GMS = "com.google.android.gms";
     public static final String PACKAGE_SETTINGS_INTEL = "com.google.android.settings.intelligence";
 
     private final Map<String, Object> mStockProperties;
     private final Map<String, Object> mFeatureProperties;
+    private Set<String> mSkipPackages = new HashSet<>();
+    private Set<String> mFeaturePackages = new HashSet<>();
+    private Set<String> mStockPackages = new HashSet<>();
 
+    private Context mContext;
     private String mPackagePrefix;
-    private String[] mSkipPackages;
-    private String[] mFeaturePackages;
-    private String[] mStockPackages;
+    private boolean mSkipImitation;
 
     public PropImitationFactory(Context context) {
-        mStockProperties = new HashMap<>();
-        mFeatureProperties = new HashMap<>();
+        mContext = context;
+
+        // List of devices that do not need imitation
+        String[] supportedDeviceList = context.getResources().getStringArray(R.array.config_propImitationSkipDeviceList);
+        mSkipImitation = Arrays.asList(supportedDeviceList).contains(SystemProperties.get("ro.product.system.device"));
 
         // Device information used for GMS
+        mStockProperties = new HashMap<>();
         final String[] stockProps = context.getResources().getStringArray(R.array.config_propImitationStockValues);
         if (stockProps.length > 0) {
             mStockProperties.put("BRAND", stockProps[0]);
@@ -64,30 +75,77 @@ public class PropImitationFactory {
             mStockProperties.put("FINGERPRINT", stockProps[5]);
         }
 
+        // Device information used to imitate
+        mFeatureProperties = new HashMap<>();
+        final String[] featureProps = context.getResources().getStringArray(R.array.config_propImitationFeatureValues);
+        if (featureProps.length > 0) {
+            mFeatureProperties.put("BRAND", featureProps[0]);
+            mFeatureProperties.put("MANUFACTURER", featureProps[1]);
+            mFeatureProperties.put("DEVICE", featureProps[2]);
+            mFeatureProperties.put("PRODUCT", featureProps[3]);
+            mFeatureProperties.put("MODEL", featureProps[4]);
+            mFeatureProperties.put("FINGERPRINT", featureProps[5]);
+        }
+
         // List of packages to process for imitation
-        mFeaturePackages = context.getResources().getStringArray(R.array.config_propImitationFeaturePackages);
-        if (mFeaturePackages.length > 0) {
-            // Device information used to imitate
-            final String[] featureProps = context.getResources().getStringArray(R.array.config_propImitationFeatureValues);
-            if (featureProps.length > 0) {
-                mFeatureProperties.put("BRAND", featureProps[0]);
-                mFeatureProperties.put("MANUFACTURER", featureProps[1]);
-                mFeatureProperties.put("DEVICE", featureProps[2]);
-                mFeatureProperties.put("PRODUCT", featureProps[3]);
-                mFeatureProperties.put("MODEL", featureProps[4]);
-                mFeatureProperties.put("FINGERPRINT", featureProps[5]);
-            }
+        String[] stockPackages = context.getResources().getStringArray(R.array.config_propImitationStockPackages);
+        if (stockPackages.length > 0) {
+            mStockPackages = new HashSet<>(Arrays.asList(stockPackages));
+        }
+
+        String[] featurePackages = context.getResources().getStringArray(R.array.config_propImitationFeaturePackages);
+        if (featurePackages.length > 0) {
+            mFeaturePackages = new HashSet<>(Arrays.asList(featurePackages));
+        }
+
+        String[] skipPackages = context.getResources().getStringArray(R.array.config_propImitationSkipPackages);
+        if (skipPackages.length > 0) {
+            mSkipPackages = new HashSet<>(Arrays.asList(skipPackages));
         }
 
         // Get the prefix of applications to filter. If empty, no apps will be processed for imitation.
         mPackagePrefix = context.getResources().getString(R.string.config_propImitationPackagePrefix);
+    }
+
+    private void setPackageProps(String packageName) {
         if (!TextUtils.isEmpty(mPackagePrefix)) {
-            // List of packages to omit from imitation
-            mSkipPackages = context.getResources().getStringArray(R.array.config_propImitationSkipPackages);
+            if (packageName.startsWith(mPackagePrefix)) {
+                if (!mStockProperties.isEmpty()) {
+                    for (Map.Entry<String, Object> prop : mStockProperties.entrySet()) {
+                        if (mSkipPackages.contains(packageName))
+                            continue;
+                        if (mFeaturePackages.contains(packageName))
+                            continue;
+                        if (DEBUG) Log.d(TAG, "Defining " + prop.getKey() + " prop for: " + packageName);
+                        setPropValue(prop.getKey(), prop.getValue());
+                    }
+                }
+            }
         }
 
-        // List of packages to process for imitation
-        mStockPackages = context.getResources().getStringArray(R.array.config_propImitationStockPackages);
+        if (mStockPackages.contains(packageName)) {
+            if (!mStockProperties.isEmpty()) {
+                for (Map.Entry<String, Object> prop : mStockProperties.entrySet()) {
+                    if (mSkipPackages.contains(packageName))
+                        continue;
+                    if (mFeaturePackages.contains(packageName))
+                        continue;
+                    if (DEBUG) Log.d(TAG, "Defining " + prop.getKey() + " prop for: " + packageName);
+                    setPropValue(prop.getKey(), prop.getValue());
+                }
+            }
+        }
+
+        if (mFeaturePackages.contains(packageName)) {
+            if (!mFeatureProperties.isEmpty()) {
+                for (Map.Entry<String, Object> prop : mFeatureProperties.entrySet()) {
+                    if (mSkipPackages.contains(packageName))
+                        continue;
+                    if (DEBUG) Log.d(TAG, "Defining " + prop.getKey() + " prop for: " + packageName);
+                    setPropValue(prop.getKey(), prop.getValue());
+                }
+            }
+        }
     }
 
     public void setProps(Application app) {
@@ -101,51 +159,23 @@ public class PropImitationFactory {
         mIsCallerPackageGms = packageName.equals(PACKAGE_GMS) &&
                 processName.equals(PACKAGE_GMS + ".unstable");
         if (mIsCallerPackageGms) {
-            if (!mStockProperties.isEmpty()) {
+            if (mSkipImitation) {
+                setPropValue("MODEL", Build.MODEL + " ");
+            } else if (!mStockProperties.isEmpty()) {
                 for (Map.Entry<String, Object> prop : mStockProperties.entrySet()) {
                     if (DEBUG) Log.d(TAG, "Defining " + prop.getKey() + " prop for: " + packageName);
-                    setPropValue(prop.getKey(), prop.getValue());
+                    if (prop.getKey().equals("MODEL")) {
+                        setPropValue(prop.getKey(), prop.getValue().toString() + " ");
+                    } else {
+                        setPropValue(prop.getKey(), prop.getValue());
+                    }
                 }
             }
-            setPropValue("MODEL", Build.MODEL + " ");
             return;
         }
 
-        if (packageName.startsWith(mPackagePrefix)) {
-            if (!mStockProperties.isEmpty()) {
-                for (Map.Entry<String, Object> prop : mStockProperties.entrySet()) {
-                    if (Arrays.asList(mSkipPackages).contains(packageName))
-                        continue;
-                    if (Arrays.asList(mFeaturePackages).contains(packageName))
-                        continue;
-                    if (DEBUG) Log.d(TAG, "Defining " + prop.getKey() + " prop for: " + packageName);
-                    setPropValue(prop.getKey(), prop.getValue());
-                }
-            }
-        }
-
-        if (Arrays.asList(mStockPackages).contains(packageName)) {
-            if (!mStockProperties.isEmpty()) {
-                for (Map.Entry<String, Object> prop : mStockProperties.entrySet()) {
-                    if (Arrays.asList(mSkipPackages).contains(packageName))
-                        continue;
-                    if (Arrays.asList(mFeaturePackages).contains(packageName))
-                        continue;
-                    if (DEBUG) Log.d(TAG, "Defining " + prop.getKey() + " prop for: " + packageName);
-                    setPropValue(prop.getKey(), prop.getValue());
-                }
-            }
-        }
-
-        if (Arrays.asList(mFeaturePackages).contains(packageName)) {
-            if (!mFeatureProperties.isEmpty()) {
-                for (Map.Entry<String, Object> prop : mFeatureProperties.entrySet()) {
-                    if (Arrays.asList(mSkipPackages).contains(packageName))
-                        continue;
-                    if (DEBUG) Log.d(TAG, "Defining " + prop.getKey() + " prop for: " + packageName);
-                    setPropValue(prop.getKey(), prop.getValue());
-                }
-            }
+        if (!mSkipImitation) {
+            setPackageProps(packageName);
         }
 
         if (packageName.equals(PACKAGE_SETTINGS_INTEL)) {
