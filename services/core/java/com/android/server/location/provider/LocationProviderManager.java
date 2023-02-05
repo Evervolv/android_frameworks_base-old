syberia-project/platform_frameworks_base

Fix wakelock under-locked issue
Check the wakelock held status before release it in the
post execute stage to avoid under-locked exception.

Bug: 213263264
Test: Monkey test
Change-Id: Iff95a01a0f49dccf6f5ecbbac4ee4306d214dae6

services/core/java/com/android/server/location/provider/LocationProviderManager.java
@@ -947,7 +947,7 @@ public class LocationProviderManager extends
 
                 @Override
                 public void onPostExecute(boolean success) {
-                    if (!success && mUseWakeLock) {
+                    if (!success && mUseWakeLock && mWakeLock.isHeld()) {
                         mWakeLock.release();
                     }
 

Evervolv/android_frameworks_base-old

services/core/java/com/android/server/location/provider/LocationProviderManager.java
@@ -1003,7 +1003,7 @@ public class LocationProviderManager extends
 
                 @Override
                 public void onPostExecute(boolean success) {
-                    if (!success && useWakeLock) {
+                    if (!success && useWakeLock && mWakeLock.isHeld()) {
                         mWakeLock.release();
                     }
 