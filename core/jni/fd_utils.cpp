LineageOS/android_frameworks_base

Add lineage sdk resource APK to Zygote FD whitelist
Change-Id: Ica91b23e467db61f142b183316db4afee2a2a283

core/jni/fd_utils.cpp
@@ -42,6 +42,7 @@ static const char* kPathWhitelist[] = {
   "/dev/urandom",
   "/dev/ion",
   "/dev/dri/renderD129", // Fixes b/31172436
+  "/system/framework/org.lineageos.platform-res.apk",
 };
 
 static const char kFdPath[] = "/proc/self/fd";

Evervolv/android_frameworks_base-old

core/jni/fd_utils.cpp
@@ -44,6 +44,7 @@ static const char* kPathAllowlist[] = {
         "/sys/kernel/debug/tracing/trace_marker",
         "/sys/kernel/tracing/trace_marker",
         "/system/framework/framework-res.apk",
+        "/system/framework/com.evervolv.platform-res.apk",
         "/dev/urandom",
         "/dev/ion",
         "/dev/dri/renderD129", // Fixes b/31172436